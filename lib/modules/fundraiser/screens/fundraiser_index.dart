34fee850a6130bbdfd7c1a9d5ad165e8623f01bc
Shoaib Fayyaz <shoaib@coinchance.io>
2025-04-18 22:29:23 +0500

    removed bloc consumer in fundraiser

--- lib/modules/fundraiser/screens/fundraiser_index.dart
+++ lib/modules/fundraiser/screens/fundraiser_index.dart
@@ -1,8 +1,6 @@
 import 'package:doneto/core/widgets/base_widget.dart';
-import 'package:doneto/modules/fundraiser/bloc/fundraiser_bloc.dart';
 import 'package:doneto/modules/onbording/widgets/my_top_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FundraiserIndex extends StatefulWidget {
   const FundraiserIndex({super.key});
@@ -17,29 +15,23 @@ class _FundraiserIndexState extends State<FundraiserIndex> {
   @override
   Widget build(BuildContext context) {
     return Background(
-      child: BlocConsumer<FundraiserBloc, FundraiserState>(
-        listener: (context, state) {},
-
-        builder: (context, state) {
-          return Column(
-            children: [
-              MyTopBar(onTap: () {}, title: ''),
-              Positioned(
-                left: 0,
-                right: 0,
-                bottom: 0,
-                top: 0,
-                child: PageView(
-                  controller: _pageController,
-                  onPageChanged: (index) {
-                    // context.read<OnboardingBloc>().add(UpdatePageEvent(page: index));
-                  },
-                  children: [],
-                ),
-              ),
-            ],
-          );
-        },
+      child: Column(
+        children: [
+          MyTopBar(onTap: () {}, title: ''),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: 0,
+            child: PageView(
+              controller: _pageController,
+              onPageChanged: (index) {
+                // context.read<OnboardingBloc>().add(UpdatePageEvent(page: index));
+              },
+              children: [],
+            ),
+          ),
+        ],
       ),
     );
   }
