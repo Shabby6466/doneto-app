aceb67d6e72bffa8cf479afe4e426e70448468aa
LENOVO <mahaayubf23@nutech.edu.pk>
2025-05-08 03:26:18 +0500

    feat: profile screen

--- lib/modules/profile/screens/profile_index.dart
+++ lib/modules/profile/screens/profile_index.dart
@@ -10,6 +10,8 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../fundraiser/widgets/doneto_button.dart';
+
 class ProfileIndex extends StatefulWidget {
   const ProfileIndex({super.key});
 
@@ -43,15 +45,103 @@ class _ProfileIndexState extends State<ProfileIndex> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     const Spacer(),
-                    Image.asset(width: 182.w, R.assets.graphics.pngIcons.donetoLogo),
+                    Image.asset(
+                      width: 182.w,
+                      R.assets.graphics.pngIcons.donetoLogo,
+                    ),
                     const Spacer(),
                     GestureDetector(
                       onTap: () {
                         context.read<AuthBloc>().add(LogoutEvent());
                       },
-                      child: SvgPicture.asset(width: 35.h, R.assets.graphics.svgIcons.redCross),
+                      child: SvgPicture.asset(
+                        width: 35.h,
+                        R.assets.graphics.svgIcons.redCross,
+                      ),
                     ),
                   ],
+                ),
+              ),
+              SizedBox(height: 25.95.h),
+              Container(
+                height: 150.h,
+                width: 150.h,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: AssetImage(R.assets.graphics.pngIcons.bgFlowers),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Faraz Asad',
+                  style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    fontSize: 24.sp,
+                    fontWeight: FontWeight.w800,
+                    color: R.palette.blackColor,
+                    height: 1.5.h,
+                  ),
+                ),
+              ),
+              SizedBox(height: 15.h),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+
+                child: Container(
+                  width: 318.w,
+
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Passionate about crisis relif \nhelp the victms of society everyone \ndeserves ,happiness',
+                    textAlign: TextAlign.center,
+                    style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w600,
+                      color: R.palette.blackColor,
+                      height: 1.4.h,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(height: 31),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  '127 profile views',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    fontSize: 13.sp,
+                    fontWeight: FontWeight.w400,
+                    color: R.palette.blackColor,
+                    height: 1.4.h,
+                  ),
+                ),
+              ),
+              SizedBox(height: 43.h),
+              Padding(
+                padding: const EdgeInsets.only(right: 113, left: 115),
+                child: DonetoButton(
+                  title: 'Share Profile',
+                  onTap: () {
+                    sl<Navigation>().push(path: Routes.fundraisingIndex);
+                  },
+                ),
+              ),
+              SizedBox(height: 28.h),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'Fundraisers I support',
+                  textAlign: TextAlign.center,
+                  style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    fontSize: 20.sp,
+                    fontWeight: FontWeight.w600,
+                    color: R.palette.blackColor,
+                    height: 1.4.h,
+                  ),
                 ),
               ),
             ],
